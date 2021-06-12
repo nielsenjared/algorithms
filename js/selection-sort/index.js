cea59837e6c7060735a0cede8c91b9b013301ffa
Jared Nielsen <nielsen.jared@gmail.com>
2021-06-12 09:00:35 -0400

    selection sort

---
+++ js/selection-sort/index.js
@@ -0,0 +1,22 @@
+const unsorted = [10, 1, 9, 2, 8, 3, 7, 4, 6, 5];
+
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let min = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        
+        let tmp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = tmp;
+    }
+    return arr;
+};
+
+const sorted = selectionSort(unsorted);
+
+console.log(sorted);
