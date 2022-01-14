4787f4cc7e26a17c463804c3f3a809dac04b4ff2
Jared Nielsen <nielsen.jared@gmail.com>
2022-01-14 16:37:37 -0500

    fizz the buzz

--- js/fizzbuzz/index.js
+++ js/fizzbuzz/index.js
@@ -1,18 +1,27 @@
-const fizzBuzz = (n) => {
-    for (let i = 1; i <= n; i++){
-        if ((i % 3 === 0) && (i % 5 === 0)) {
-            console.log(i, "Fizz Buzz");
-        }
-        else if (i % 3 === 0) {
-            console.log(i, "Fizz");
-        }
-        else if (i % 5 === 0) {
-            console.log(i, "Buzz");
-        }
-        else {
-            console.log(i);
-        }
+// const fizzBuzz = (n) => {
+//     for (let i = 1; i <= n; i++){
+//         if ((i % 3 === 0) && (i % 5 === 0)) {
+//             console.log(i, "Fizz Buzz");
+//         }
+//         else if (i % 3 === 0) {
+//             console.log(i, "Fizz");
+//         }
+//         else if (i % 5 === 0) {
+//             console.log(i, "Buzz");
+//         }
+//         else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// fizzBuzz(20);
+
+const fB = (n) => {
+    for(let i = 0; i < n;) {
+        console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
     }
 }
 
-fizzBuzz(20);
+fB(20);
+
