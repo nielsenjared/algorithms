a0630bdae2c22af66e8de36ed042c139640411b5
jared nielsen <nielsen.jared@gmail.com>
2022-11-14 11:36:24 -0500

    update combos

--- 25-combinations/index.js
+++ 25-combinations/index.js
@@ -1,46 +1,43 @@
 const combinations = (n, k) => {
   const combos = [];
   
-  let head, tails;
+  let head, tail;
   
-  if (k > n.length || k < 1) { 
-    return []; 
-  }
+  // if (k > n.length || k < 1) { 
+  //   return []; 
+  // }
   
-  if (k === n.length) { 
-    return [ n ]; 
-  }
+  // if (k === n.length) { 
+  //   return [ n ]; 
+  // }
   
   if (k === 1) {
-    for (let i = 0; i < n.length; i++) {
-      combos.push([n[i]]);
-    }
-    return combos;
+    console.log(n);
+    return n;
+    // for (let i = 0; i < n.length; i++) {
+    //   combos.push([n[i]]);
+    // }
+    // return combos;
   }
 
-  console.log(n.length-k)
 
   for (let i = 0; i < n.length; i++) {
   // for (let i = 0; i < n.length - k + 1; i++) {
       head = n.slice(i, i + 1);
 
-      console.log(head);
+      tail = combinations(n.slice(i + 1), k - 1);
 
-      console.log(n.slice(i + 1), k - 1)
-
-      tails = combinations(n.slice(i + 1), k - 1);
-
-      console.log("tails " + tails);
-
-      for (let j = 0; j < tails.length; j++) {
-        let combo = head.concat(tails[j]);
+      for (let j = 0; j < tail.length; j++) {
+        let combo = head.concat(tail[j]);
         combos.push(combo);
-        // combos.push(head.concat(tails[j]));
+        // combos.push(head.concat(tail[j]));
       }
   }
 
   return combos; 
 }
+
+
 
 const array = [1, 2, 3, 4, 5]
 
