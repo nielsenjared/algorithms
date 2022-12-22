22ad8f07c5a5e93b46e5d2d96873ec541c131acb
Jared Nielsen <nielsen.jared@gmail.com>
2022-12-22 16:25:25 -0500

    refactor LIS

--- 12-longest-increasing-subsequence/index.js
+++ 12-longest-increasing-subsequence/index.js
@@ -1,25 +1,24 @@
 const longestIncreasingSubsequence = (n) => {
-    const lengths = new Array(n.length).fill(1);
-    let result = 0;
+    const tally = [1];
+    let result = 1;
     
     for (let i = 1; i < n.length; i++) {
+        tally[i] = 1;
         for (let j = 0; j < i; j++) {
-            let current = lengths[j] + 1;
+            let current = tally[j] + 1;
             
-            if (n[j] < n[i] && current > lengths[i]) {
-                lengths[i] = current
+            if (n[j] < n[i] && current > tally[i]) {
+                tally[i] = current
                 if (current > result) {
-                    
                     result = current;
                 }
             }
         }
     }
-    // return Math.max(...lengths);
     return result; 
 }
 
-const arr = [1, 4, 1, 5, 9, 2, 6, 5], 3, 5, 8, 9, 7, 9, 3, 2];
+const arr = [1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2];
 
 let result = longestIncreasingSubsequence(arr);
 
--- 12-longest-increasing-subsequence/main.py
+++ 12-longest-increasing-subsequence/main.py
@@ -1,20 +1,21 @@
 def longest_increasing_subsequence(n):
-    lengths = [1 for i in range(len(n))]
+    tally = [1]
     result = 0
     
     for i in range(1, len(n)):
+        tally += [1]
         for j in range(i):
-            current = lengths[j] + 1
+            current = tally[j] + 1
 
-            if (n[j] < n[i] and current > lengths[i]):
-                lengths[i] = current
+            if (n[j] < n[i] and current > tally[i]):
+                tally[i] = current
 
                 if current > result:
                     result = current
                     
     return result
 
-list = [1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2];
+list = [1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2]
 
 result = longest_increasing_subsequence(list)
 
